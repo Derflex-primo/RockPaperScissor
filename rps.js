60a24cb3d91a5edbdff17a4b73333c0c73833209
Daryl Deogracias <daryldeogracias@Daryls-MacBook-Air.local>
2023-07-02 04:14:05 +0800

    initialized git repository

---
+++ rps.js
@@ -0,0 +1,52 @@
+const playerText = document.querySelector("#playerText");
+const computerText = document.querySelector("#computerText");
+const resultText = document.querySelector("#resultText");
+const choiceBtns = document.querySelectorAll(".choiceBtn");
+
+
+let player;
+let computer;
+let result;
+
+choiceBtns.forEach(button => button.addEventListener("click", () => {
+    player = button.textContent;
+    computerTurn();
+    playerText.textContent = `Player: ${player}`;
+    computerText.textContent = `Computer: ${computer}`;
+    resultText.textContent =  checkWinner();
+}))
+
+function computerTurn(){
+    const randNum = Math.floor(Math.random() * 3) + 1;
+
+    switch(randNum){
+        case 1:
+            computer = "ROCK"
+            break;
+        case 2:
+            computer = "PAPER"
+            break;
+        case 3:
+            computer = "SCISSOR"
+            break;
+    }
+
+     
+}
+
+function checkWinner(){
+  if(player === computer){
+    return "IT'S A DRAW!"; 
+  }
+  else if(computer === "ROCK"){
+    return (player ==="PAPER") ? "You win!" : "You lose HAHAHA!";
+  }
+  else if(computer === "PAPER"){
+    return (player === "SCISSOR") ? "You win!" : "You lose HAHAHA!";
+  }
+  else if(computer === "SCISSOR"){
+    return (player === "ROCK") ? "You win!" : "You lose HAHAHA!";
+  }
+
+  
+}
